913a2eef071e00b7de4d00f907df28a5ed103bcc
Chris Beams <cbeams@vmware.com>
2011-01-18 18:51:19 +0700

    Eliminate ComponentScanElementSpecificationCreator

--- org.springframework.context/src/main/java/org/springframework/context/annotation/ComponentScanBeanDefinitionParser.java
+++ org.springframework.context/src/main/java/org/springframework/context/annotation/ComponentScanBeanDefinitionParser.java
@@ -16,19 +16,33 @@
 
 package org.springframework.context.annotation;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
+import java.util.regex.Pattern;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.FatalBeanException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.parsing.BeanComponentDefinition;
 import org.springframework.beans.factory.parsing.CompositeComponentDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.BeanDefinitionParserDelegate;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.beans.factory.xml.XmlReaderContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.ExecutorContext;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.AspectJTypeFilter;
+import org.springframework.core.type.filter.AssignableTypeFilter;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Parser for the &lt;context:component-scan/&gt; element. Parsed metadata is
@@ -41,30 +55,47 @@ import org.w3c.dom.Element;
  * @author Juergen Hoeller
  * @author Chris Beams
  * @since 2.5
- * @see ComponentScanExecutor
  * @see ComponentScan
- * @see ComponentScanAnnotationSpecificationCreator
+ * @see ComponentScanSpecification
+ * @see ComponentScanExecutor
  */
 public class ComponentScanBeanDefinitionParser implements BeanDefinitionParser {
+
+	private static final String BASE_PACKAGE_ATTRIBUTE = "base-package";
+
+	private static final String RESOURCE_PATTERN_ATTRIBUTE = "resource-pattern";
+
+	private static final String USE_DEFAULT_FILTERS_ATTRIBUTE = "use-default-filters";
+
+	private static final String ANNOTATION_CONFIG_ATTRIBUTE = "annotation-config";
+
+	private static final String NAME_GENERATOR_ATTRIBUTE = "name-generator";
+
+	private static final String SCOPE_RESOLVER_ATTRIBUTE = "scope-resolver";
+
+	private static final String SCOPED_PROXY_ATTRIBUTE = "scoped-proxy";
+
+	private static final String EXCLUDE_FILTER_ELEMENT = "exclude-filter";
+
+	private static final String INCLUDE_FILTER_ELEMENT = "include-filter";
+
+	private static final String FILTER_TYPE_ATTRIBUTE = "type";
+
+	private static final String FILTER_EXPRESSION_ATTRIBUTE = "expression";
+
 
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
 		XmlReaderContext readerContext = parserContext.getReaderContext();
 		BeanDefinitionParserDelegate delegate = parserContext.getDelegate();
 		BeanDefinitionRegistry registry = parserContext.getRegistry();
 
-		ComponentScanElementSpecificationCreator specCreator = new ComponentScanElementSpecificationCreator(parserContext);
-		ComponentScanSpecification spec = specCreator.createFrom(element);
+		ComponentScanSpecification spec = createSpecification(element, parserContext);
 
 		ComponentScanExecutor specExecutor = new ComponentScanExecutor();
 		specExecutor.setBeanDefinitionDefaults(delegate.getBeanDefinitionDefaults());
 		specExecutor.setAutowireCandidatePatterns(delegate.getAutowireCandidatePatterns());
 
-		ExecutorContext executorContext = new ExecutorContext();
-		executorContext.setRegistry(registry);
-		executorContext.setResourceLoader(readerContext.getResourceLoader());
-		executorContext.setEnvironment(delegate.getEnvironment());
-
-		specExecutor.execute(spec, executorContext);
+		specExecutor.execute(spec, createExecutorContext(parserContext));
 
 		Object source = readerContext.extractSource(element);
 		CompositeComponentDefinition compositeDef = new CompositeComponentDefinition(element.getTagName(), source);
@@ -86,5 +117,151 @@ public class ComponentScanBeanDefinitionParser implements BeanDefinitionParser {
 		return null;
 	}
 
+	private ExecutorContext createExecutorContext(ParserContext parserContext) {
+		XmlReaderContext readerContext = parserContext.getReaderContext();
+		BeanDefinitionParserDelegate delegate = parserContext.getDelegate();
+		BeanDefinitionRegistry registry = parserContext.getRegistry();
+
+		ExecutorContext executorContext = new ExecutorContext();
+		executorContext.setRegistry(registry);
+		executorContext.setResourceLoader(readerContext.getResourceLoader());
+		executorContext.setEnvironment(delegate.getEnvironment());
+		return executorContext;
+	}
+
+	protected ComponentScanSpecification createSpecification(Element element, ParserContext parserContext) {
+		XmlReaderContext readerContext = parserContext.getReaderContext();
+
+		ComponentScanSpecification spec = ComponentScanSpecification.withBasePackages(
+				StringUtils.tokenizeToStringArray(element.getAttribute(BASE_PACKAGE_ATTRIBUTE),
+						ConfigurableApplicationContext.CONFIG_LOCATION_DELIMITERS));
+
+		if (element.hasAttribute(ANNOTATION_CONFIG_ATTRIBUTE)) {
+			spec.includeAnnotationConfig(Boolean.valueOf(element.getAttribute(ANNOTATION_CONFIG_ATTRIBUTE)));
+		}
+
+		if (element.hasAttribute(USE_DEFAULT_FILTERS_ATTRIBUTE)) {
+			spec.useDefaultFilters(Boolean.valueOf(element.getAttribute(USE_DEFAULT_FILTERS_ATTRIBUTE)));
+		}
+
+		if (element.hasAttribute(RESOURCE_PATTERN_ATTRIBUTE)) {
+			spec.resourcePattern(element.getAttribute(RESOURCE_PATTERN_ATTRIBUTE));
+		}
+
+		if (element.hasAttribute(NAME_GENERATOR_ATTRIBUTE)) {
+			spec.beanNameGenerator(instantiateUserDefinedStrategy(
+					element.getAttribute(NAME_GENERATOR_ATTRIBUTE), BeanNameGenerator.class,
+					readerContext.getResourceLoader().getClassLoader()));
+		}
+
+
+		// Register ScopeMetadataResolver if class name provided.
+		if (element.hasAttribute(SCOPE_RESOLVER_ATTRIBUTE)) {
+			if (element.hasAttribute(SCOPED_PROXY_ATTRIBUTE)) {
+				throw new IllegalArgumentException(
+						"Cannot define both 'scope-resolver' and 'scoped-proxy' on <component-scan> tag");
+			}
+			ScopeMetadataResolver scopeMetadataResolver = instantiateUserDefinedStrategy(
+					element.getAttribute(SCOPE_RESOLVER_ATTRIBUTE), ScopeMetadataResolver.class,
+					readerContext.getResourceLoader().getClassLoader());
+			spec.scopeMetadataResolver(scopeMetadataResolver);
+		}
+
+		if (element.hasAttribute(SCOPED_PROXY_ATTRIBUTE)) {
+			String mode = element.getAttribute(SCOPED_PROXY_ATTRIBUTE);
+			if ("targetClass".equals(mode)) {
+				spec.scopedProxyMode(ScopedProxyMode.TARGET_CLASS);
+			}
+			else if ("interfaces".equals(mode)) {
+				spec.scopedProxyMode(ScopedProxyMode.INTERFACES);
+			}
+			else if ("no".equals(mode)) {
+				spec.scopedProxyMode(ScopedProxyMode.NO);
+			}
+			else {
+				throw new IllegalArgumentException("scoped-proxy only supports 'no', 'interfaces' and 'targetClass'");
+			}
+		}
+
+		// Parse exclude and include filter elements.
+		ClassLoader classLoader = readerContext.getResourceLoader().getClassLoader();
+		NodeList nodeList = element.getChildNodes();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				String localName = parserContext.getDelegate().getLocalName(node);
+				try {
+					if (INCLUDE_FILTER_ELEMENT.equals(localName)) {
+						TypeFilter typeFilter = createTypeFilter((Element) node, classLoader);
+						spec.addIncludeFilter(typeFilter);
+					}
+					else if (EXCLUDE_FILTER_ELEMENT.equals(localName)) {
+						TypeFilter typeFilter = createTypeFilter((Element) node, classLoader);
+						spec.addExcludeFilter(typeFilter);
+					}
+				}
+				catch (Exception ex) {
+					readerContext.error(ex.getMessage(), readerContext.extractSource(element), ex.getCause());
+				}
+			}
+		}
+
+		return spec;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected TypeFilter createTypeFilter(Element element, ClassLoader classLoader) {
+		String filterType = element.getAttribute(FILTER_TYPE_ATTRIBUTE);
+		String expression = element.getAttribute(FILTER_EXPRESSION_ATTRIBUTE);
+		try {
+			if ("annotation".equals(filterType)) {
+				return new AnnotationTypeFilter((Class<Annotation>) classLoader.loadClass(expression));
+			}
+			else if ("assignable".equals(filterType)) {
+				return new AssignableTypeFilter(classLoader.loadClass(expression));
+			}
+			else if ("aspectj".equals(filterType)) {
+				return new AspectJTypeFilter(expression, classLoader);
+			}
+			else if ("regex".equals(filterType)) {
+				return new RegexPatternTypeFilter(Pattern.compile(expression));
+			}
+			else if ("custom".equals(filterType)) {
+				Class<?> filterClass = classLoader.loadClass(expression);
+				if (!TypeFilter.class.isAssignableFrom(filterClass)) {
+					throw new IllegalArgumentException(
+							"Class is not assignable to [" + TypeFilter.class.getName() + "]: " + expression);
+				}
+				return (TypeFilter) BeanUtils.instantiateClass(filterClass);
+			}
+			else {
+				throw new IllegalArgumentException("Unsupported filter type: " + filterType);
+			}
+		}
+		catch (ClassNotFoundException ex) {
+			throw new FatalBeanException("Type filter class not found: " + expression, ex);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> T instantiateUserDefinedStrategy(String className, Class<T> strategyType, ClassLoader classLoader) {
+		Object result = null;
+		try {
+			result = classLoader.loadClass(className).newInstance();
+		}
+		catch (ClassNotFoundException ex) {
+			throw new IllegalArgumentException("Class [" + className + "] for strategy [" +
+					strategyType.getName() + "] not found", ex);
+		}
+		catch (Exception ex) {
+			throw new IllegalArgumentException("Unable to instantiate class [" + className + "] for strategy [" +
+					strategyType.getName() + "]. A zero-argument constructor is required", ex);
+		}
+
+		if (!strategyType.isAssignableFrom(result.getClass())) {
+			throw new IllegalArgumentException("Provided class name must be an implementation of " + strategyType);
+		}
+		return (T)result;
+	}
 
 }
