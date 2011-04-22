448bf9eaf247dca4a57f2b6151a7e1db388642d9
Chris Beams <cbeams@vmware.com>
2011-04-22 18:59:29 +0800

    Introduce @EnableAsync

---
+++ org.springframework.context/src/test/java/org/springframework/scheduling/annotation/EnableAsyncTests.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.scheduling.annotation;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Tests use of @EnableAsync on @Configuration classes.
+ *
+ * @author Chris Beams
+ * @since 3.1
+ */
+public class EnableAsyncTests {
+
+	@Test
+	public void proxyingOccurs() {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(AsyncConfig.class);
+		ctx.refresh();
+
+		AsyncBean asyncBean = ctx.getBean(AsyncBean.class);
+		assertThat(AopUtils.isAopProxy(asyncBean), is(true));
+		asyncBean.work();
+	}
+
+
+	@Configuration
+	@EnableAsync
+	static class AsyncConfig {
+		@Bean
+		public AsyncBean asyncBean() {
+			return new AsyncBean();
+		}
+	}
+
+
+	static class AsyncBean {
+		@Async
+		public void work() {
+		}
+	}
+}
