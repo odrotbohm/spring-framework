784a4761edf8af22de9d0dbd43b7059d92a10c95
Chris Beams <cbeams@vmware.com>
2011-05-13 19:49:33 +0800

    Add test in preparation for EMFB refactoring

---
+++ org.springframework.orm/src/test/java/org/springframework/orm/jpa/eclipselink/EntityManagerFactoryConfig.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2002-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.orm.jpa.eclipselink;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+
+@Configuration
+public class EntityManagerFactoryConfig {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Bean(name="entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean fromFactory() {
+		LocalContainerEntityManagerFactoryBean fb = new LocalContainerEntityManagerFactoryBean();
+		fb.setPersistenceXmlLocation("org/springframework/orm/jpa/domain/persistence.xml");
+		fb.setDataSource(dataSource);
+		fb.setJpaVendorAdapter(
+			new EclipseLinkJpaVendorAdapter()
+				.setDatabase(Database.HSQL)
+				.setShowSql(true)
+				.setGenerateDdl(true));
+		return fb;
+	}
+
+	/*
+	@Bean(name="entityManagerFactory")
+	public EntityManagerFactory fromBuilder() {
+		LocalContainerEntityManagerBuilder builder = new LocalContainerEntityManagerBuilder();
+		builder.setPersistenceXmlLocation("org/springframework/orm/jpa/domain/persistence.xml");
+		builder.setDataSource(dataSource);
+		builder.setJpaVendorAdapter(
+			new EclipseLinkJpaVendorAdapter()
+				.setDatabase(Database.HSQL)
+				.setShowSql(true)
+				.setGenerateDdl(true)
+		);
+	}
+	*/
+}
