27e756fd052711d41a4fbda9ce8f2ecdabd907d3
Oliver Gierke <info@olivergierke.de>
2012-02-28 15:47:04 +0100

    SPR-9177 - Show looking up FactoryBeans by type does not work for type predicted beans that have not been instantiated yet.

--- spring-beans/src/test/java/org/springframework/beans/factory/support/Spr8954Tests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/support/Spr8954Tests.java
@@ -57,9 +57,25 @@ public class Spr8954Tests {
 		Map<String, AnInterface> aiBeans = bf.getBeansOfType(AnInterface.class);
 		assertThat(1, equalTo(aiBeans.size()));
 		assertThat("&foo", equalTo(aiBeans.keySet().iterator().next()));
+	}
 
-		Map<String, PredictedType> predictedBeans = bf.getBeansOfType(PredictedType.class);
-		assertThat(predictedBeans.size(), is(1));
+	@Test
+	public void findsBeansByTypeIfNotInstantiated() {
+
+		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
+		bf.registerBeanDefinition("foo", new RootBeanDefinition(FooFactoryBean.class));
+		bf.addBeanPostProcessor(new PredictingBPP());
+
+		assertThat(bf.isTypeMatch("&foo", FactoryBean.class), is(true));
+
+		@SuppressWarnings("rawtypes")
+		Map<String, FactoryBean> fbBeans = bf.getBeansOfType(FactoryBean.class);
+		assertThat(1, equalTo(fbBeans.size()));
+		assertThat("&foo", equalTo(fbBeans.keySet().iterator().next()));
+
+		Map<String, AnInterface> aiBeans = bf.getBeansOfType(AnInterface.class);
+		assertThat(1, equalTo(aiBeans.size()));
+		assertThat("&foo", equalTo(aiBeans.keySet().iterator().next()));
 	}
 
 	static class FooFactoryBean implements FactoryBean<Foo>, AnInterface {
